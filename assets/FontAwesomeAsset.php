ac8830968240f2187593b2ad634efce6330b6f08
suryana <sheillendra@yahoo.com>
2014-09-21 05:42:43 +0700

    Update FontAwesomeAsset.php
    alias change to @bower

--- assets/FontAwesomeAsset.php
+++ assets/FontAwesomeAsset.php
@@ -16,7 +16,7 @@ use yii\web\AssetBundle;
  */
 class FontAwesomeAsset extends AssetBundle {
 
-    public $sourcePath = '@vendor/fortawesome/font-awesome';
+    public $sourcePath = '@bower/font-awesome';
     public $css = [
         'css/font-awesome.min.css',
     ];
